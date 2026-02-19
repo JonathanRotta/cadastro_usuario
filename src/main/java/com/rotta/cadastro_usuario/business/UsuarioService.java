837df2753bdf11d09f8c23b53bb411274dca124b
Jonathan Rotta <jrotta19@gmail.com>
2026-02-19 19:02:24 -0300

    Criando repositorys e services

--- src/main/java/com/rotta/cadastro_usuario/business/UsuarioService.java
+++ src/main/java/com/rotta/cadastro_usuario/business/UsuarioService.java
@@ -16,5 +16,25 @@ public class UsuarioService {
     public void SalvarUsuario(Usuario usuario){
         repository.saveAndFlush(usuario);
     }
-    
+
+    public Usuario buscarUsuarioPorEmail(String email){
+        return repository.findByEmail(email).orElseThrow(
+                () -> new RuntimeException("Email não encontrado")
+        );
+
+    }
+
+    public void deletarUsuarioPorEmail(String email){
+        repository.deleteByEmail(email);
+    }
+
+    public void atualizarUsuarioPorId(Integer id, Usuario usuario){
+        Usuario usuarioEntity = repository.findById(id).orElseThrow(() ->
+                new RuntimeException("Usuario não encontrado"));
+        Usuario usuarioAtualizado = Usuario.builder()
+                .email(usuario.getEmail() != null ? usuario.getEmail() : usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ? usuario.getNome() : usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
+    }
 }
--- src/main/java/com/rotta/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/com/rotta/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
@@ -2,6 +2,14 @@ package com.rotta.cadastro_usuario.infrastructure.repository;
 
 import com.rotta.cadastro_usuario.infrastructure.entitys.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario,Integer> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
 }
